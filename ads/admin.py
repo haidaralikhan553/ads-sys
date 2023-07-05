21ed3181e4446e718d5badb96ef5b096c4ccbae6
Haidar Ali <haidar.alikhan553@gmail.com>
2023-07-06 00:18:00 +0500

    counter moved to seperate model

--- ads/admin.py
+++ ads/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Ads
+from .models import Ads, AdviewCounter
 
 admin.site.register(Ads)
+admin.site.register(AdviewCounter)
--- ads/models.py
+++ ads/models.py
@@ -6,14 +6,23 @@ class Ads(models.Model):
     start_date = models.DateField(auto_now=True)
     end_date = models.DateField(auto_now=True)
 
+    def __str__(self) -> str:
+        return self.ad_name
+
     def save(self, *args, **kwargs):
         is_new_ad = not self.pk
         super().save(*args, **kwargs)
         if is_new_ad:
-            UserProfile.objects.create(ad=self)
+            AdviewCounter.objects.create(ad=self, count=0, max_count=200, location="khi")
+            AdviewCounter.objects.create(ad=self, count=0, max_count=1000, location="mul")
+            AdviewCounter.objects.create(ad=self, count=0, max_count=100, location="lhr")
 
 
 class AdviewCounter(models.Model):
-    ad = models.ForeignKey(Ads, ondelete=models.CASCADE)
+    ad = models.ForeignKey(Ads, on_delete=models.CASCADE)
     count = models.IntegerField(default=0, null=True, blank=True)
+    max_count = models.IntegerField(default=1000, null=True, blank=True)
     location = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.ad.ad_name + " : " + str(self.count)
